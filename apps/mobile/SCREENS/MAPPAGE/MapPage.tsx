0d43d78ac6752284287866a3899a76913b42b5c3
DragonNguyen926 <duonglong09022006@gmail.com>
2026-02-07 09:43:49 -0800

    feat(ui): implement Home page and Event creation UI

---
+++ apps/mobile/SCREENS/MAPPAGE/MapPage.tsx
@@ -0,0 +1,10 @@
+import React from "react"
+import { View, Text } from "react-native"
+
+export function MapPage() {
+  return (
+    <View style={{ flex: 1, alignItems: "center", justifyContent: "center" }}>
+      <Text>Map Page</Text>
+    </View>
+  )
+};
