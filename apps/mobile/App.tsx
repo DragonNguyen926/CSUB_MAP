22c81212d8cac26c2104299cf95045410da5409b
Anne Le <annele@stg-edu-136-168-59-63.dyn.csub.edu>
2026-02-27 18:15:38 -0800

    QR code scanner & dark mode added

--- apps/mobile/App.tsx
+++ apps/mobile/App.tsx
@@ -2,13 +2,16 @@ import React from "react";
 import { SafeAreaProvider } from "react-native-safe-area-context";
 import { NavigationContainer } from "@react-navigation/native";
 import { NavBar } from "./SCREENS/NAV/NavBar";
+import { ThemeProvider } from "./SCREENS/NAV/ThemeProvider";
 
 export default function App() {
   return (
     <SafeAreaProvider>
+      <ThemeProvider>
         <NavigationContainer>
-        <NavBar />
-      </NavigationContainer>
+          <NavBar />
+        </NavigationContainer>
+      </ThemeProvider>
     </SafeAreaProvider>
   );
 }
